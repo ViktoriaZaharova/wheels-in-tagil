a89cfd2abcf28d59e87be5775d5db4fbd0014333
Viktoria <zaharova181114@gmail.com>
2021-02-24 00:29:08 +0200

    mobile 1024

--- app/js/common.js
+++ app/js/common.js
@@ -9,6 +9,14 @@ $('.sales-slider').slick({
     nextArrow: '<button type="button" class="slick-next"><svg width="17" height="10" viewBox="0 0 17 10" xmlns="http://www.w3.org/2000/svg">\n' +
         '        <path d="M11.3704 1L15 5M15 5L11.3704 9M15 5L-1.74846e-07 5" stroke-width="2"/>\n' +
         '    </svg></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 // mask
@@ -28,3 +36,57 @@ $('.tooltip-btn').on('click', function () {
 $('.tooltip-box .btn-close').on('click', function () {
     $(this).parents('.tooltip-box').fadeOut();
 });
+
+// menu mobile dropdown
+var autocollapse = function (menu,maxHeight) {
+
+    var nav = $(menu);
+    var navHeight = nav.innerHeight();
+    if (navHeight >= maxHeight) {
+
+        $(menu + ' .dropdown').removeClass('d-none');
+        $(".header-menu").removeClass('w-auto').addClass("w-100");
+
+        while (navHeight > maxHeight) {
+            //  add child to dropdown
+            var children = nav.children(menu + ' li:not(:last-child)');
+            var count = children.length;
+            $(children[count - 1]).prependTo(menu + ' .dropdown-menu');
+            navHeight = nav.innerHeight();
+        }
+        $(".header-menu").addClass("w-auto").removeClass('w-100');
+
+    }
+    else {
+
+        var collapsed = $(menu + ' .dropdown-menu').children(menu + ' li');
+
+        if (collapsed.length===0) {
+            $(menu + ' .dropdown').addClass('d-none');
+        }
+
+        while (navHeight < maxHeight && (nav.children(menu + ' li').length > 0) && collapsed.length > 0) {
+            //  remove child from dropdown
+            collapsed = $(menu + ' .dropdown-menu').children('li');
+            $(collapsed[0]).insertBefore(nav.children(menu + ' li:last-child'));
+            navHeight = nav.innerHeight();
+        }
+
+        if (navHeight > maxHeight) {
+            autocollapse(menu,maxHeight);
+        }
+    }
+}
+
+$(document).ready(function () {
+
+    // when the page loads
+    autocollapse('#nav',25);
+
+    // when the window is resized
+    $(window).on('resize', function () {
+        autocollapse('#nav',25);
+    });
+
+});
+
